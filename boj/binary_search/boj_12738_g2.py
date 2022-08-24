c3a47bb42bd22c9311d0be6e2ecf79dc37a8a45a
BrianDYKim <18k7102dy@naver.com>
2022-08-25 02:35:37 +0900

    12738 (최장 증가 부분수열) 문제 해결

--- boj/binary_search/boj_12738_g2.py
+++ boj/binary_search/boj_12738_g2.py
@@ -3,5 +3,31 @@ input = sys.stdin.readline
 
 N = int(input())
 num_list = list(map(int, input().split()))
-num_set = set(num_list)
-print(len(num_set))
+
+# 이진탐색 메소드
+def search(start, end, target):
+    if start > end:
+        return start
+
+    mid = (start + end) // 2
+
+    if res[mid] == target:
+        return mid
+    elif res[mid] > target:
+        return search(start, mid - 1, target)
+    else:
+        return search(mid + 1, end, target)
+
+# LIS를 저장하는 배열
+res = [num_list[0]]
+
+# num_list를 첫번째 인덱스부터 순차적으로 탐색
+for i in range(1, len(num_list)):
+    # LIS의 마지막 원소와 비교를 수행한다
+    length = len(res)
+    if res[length - 1] < num_list[i]:
+        res.append(num_list[i])
+    else:
+        res[search(0, length - 1, num_list[i])] = num_list[i]
+
+print(len(res))
