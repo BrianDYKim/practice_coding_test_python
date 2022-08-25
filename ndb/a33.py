9d6c2006b4168d21afb56b6354722b690b5ed9ba
BrianDYKim <18k7102dy@naver.com>
2022-08-25 22:56:04 +0900

    a33번 문제 해결중

--- ndb/a33.py
+++ ndb/a33.py
@@ -15,5 +15,22 @@ for i in range(N):
     if i + time - 1 < N:
         schedule_list[i + time - 1].append((time, money))
 
-for schedule in schedule_list:
-    print(schedule)
+for i, schedule in enumerate(schedule_list):
+    # 스케쥴이 없는 경우
+    if len(schedule) == 0:
+        # 만약에 첫 날인경우
+        if i == 0:
+            dp_table[i] = 0
+        # 첫날이 아닌 경우에는 이전의 날이 최선이다
+        else:
+            dp_table[i] = dp_table[i - 1]
+    # 스케쥴이 있는 경우
+    else:
+        # 스케쥴 계산
+        sequence = []
+        for day, time in schedule:
+            sequence.append(dp_table[i - day] + time)
+        # 최대값을 dp_table에 할당한다
+        dp_table[i] = max(sequence)
+
+print(dp_table[N - 1])
